7a62ea9b91ccefeaf0deebccadf0eca0baa1e42f
Oveln <oveln@outlook.com>
2023-12-27 19:37:38 +0800

    Wezterm Config

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,21 @@
+local wezterm = require("wezterm")
+local config = wezterm.config_builder()
+
+config.color_scheme = "Afterglow"
+config.enable_tab_bar = false
+-- config.tab_bar_at_bottom = true
+config.window_background_opacity = 0.95
+
+-- font config
+config.font = wezterm.font("JetBrainsMono Nerd Font Mono")
+config.font_size = 13.0
+config.line_height = 0.9
+config.cell_width = 1
+
+config.initial_cols = 120
+config.initial_rows = 50
+
+config.default_prog = { "wsl.exe" }
+config.default_cwd = "//wsl$/Ubuntu/home/oveln"
+
+return config
