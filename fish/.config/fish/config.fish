d80716893d65268c15a8ed9af252e992c7abbf67
Oveln <oveln@outlook.com>
2024-02-26 21:37:16 +0800

    fish config LANG
    set LANGUAGE to en_US

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,14 +2,15 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
-set LANG en_US
 set EDITOR nvim
 # proxy
 switch $hostname
     case Oveln-PC
         set -x proxy_url http://172.27.192.1:7890
     case Oveln-Laptop
-	set -x proxy_url http://localhost:7890
+        set -x proxy_url http://localhost:7890
+    set LANG "en_US.UTF-8"
+    set LANGUAGE "en_US"
 end
 set -x http_proxy $proxy_url
 set -x https_proxy $http_proxy
